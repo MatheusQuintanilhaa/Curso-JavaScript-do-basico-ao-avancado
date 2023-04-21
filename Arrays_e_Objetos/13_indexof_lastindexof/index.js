e09a43a8470d70e0427a6b543d0e896b19544717
Matheus <matheussantos.quintanilha@gmail.com>
2023-04-20 22:49:24 -0300

    atualizando o conteudo de arrays e objetos até a aula 145 de foreach

--- Arrays_e_Objetos/13_indexof_lastindexof/index.js
+++ Arrays_e_Objetos/13_indexof_lastindexof/index.js
@@ -1,5 +1,11 @@
-let nums = [5, 6, 2, 4, 9, 6, 2];
+let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log(nums.indexOf(9));
+// console.log(nums.slice(4, 5));
+// console.log(nums.slice(4, 6));
+// console.log(nums.slice(6));
+// console.log(nums.slice(-2));
+console.log(nums.slice(3, -2));
 
-console.log(nums.lastIndexOf(2));
+// let sports = ["basquete", "futebol", "handball", "futebol-americano", "baseball", "tenis", "golf"]
+
+// console.log(sports.slice(3,5));
---
+++ Arrays_e_Objetos/15_foreach/index.js
@@ -0,0 +1,11 @@
+let nomes = ["Matheus", "Maria", "José", "Ricardo", "Adriana", "Joelson"];
+
+nomes.forEach(nome => {
+    console.log("O nome é " + nome);
+});
+
+const palavrasAleatorias = ["banana", "computador", "abacate", "bicicleta", "elefante", "geladeira", "abacaxi", "livro", "melancia", "camiseta"];
+
+palavrasAleatorias.forEach((palavra) => {
+    console.log("A palavra é: " + palavra);
+});
