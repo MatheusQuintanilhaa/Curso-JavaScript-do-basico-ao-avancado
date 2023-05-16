aa9a473403a064df7375b2f7f4cf5e5624280150
Matheus <matheussantos.quintanilha@gmail.com>
2023-05-16 20:36:48 -0300

    orientacoes_a_objetos metodo_01

---
+++ Orientacoes_a_Objetos/01_metodos/scripts.js
@@ -0,0 +1,81 @@
+const car = {
+  acessories: function () {
+    console.log('airfoil');
+  },
+  paint: function () {
+    console.log('metalic');
+  }
+}
+
+
+car.acessories();
+car.paint();
+
+
+const guitar = {
+  notes: function ()  {
+    console.log("C SHARP");
+  },
+  price: function () {
+    console.log("The value's guitar is $3.000,00");
+  }
+}
+
+guitar.notes();
+guitar.price();
+
+const movies = {
+  gender: function () {
+    console.log("comedy");
+  },
+  actor: function () {
+    console.log("Charlie Sheen");
+  },
+  serie: function () {
+    console.log("Two and a half man");
+  },
+  addiction: function () {
+    console.log("drugs");
+  }
+};
+
+
+movies.gender();
+movies.actor();
+movies.serie();
+movies.addiction();
+
+const daily = {
+  hobbie: function () {
+    console.log("Play eletric guitar and shred solos");
+  },
+  job: function () {
+    console.log("My job in this moment is Developer");
+  },
+  goal: function () {
+    console.log("Fulfill my goals before the year ends");
+  }
+};
+
+daily.hobbie();
+daily.job();
+daily.goal();
+
+const musics = {
+  gender: function () {
+    console.log("Rock");
+  },
+  music: function () {
+    console.log('April fool');
+    console.log('Anastasia');
+  },
+  bands: function () {
+    console.log('Slash and the conspirators');
+    console.log('Alter Bridge');
+    console.log('John Mayer');
+  }
+}
+
+musics.gender();
+musics.music();
+musics.bands();
